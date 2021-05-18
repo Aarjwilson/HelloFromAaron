8cecb1994d4d519f79da0287da81a1f81e793470
Abram Ephraim <eph20001@byui.edu>
2021-05-17 19:48:30 -0600

    Added a line to say hello to Aaron.

--- src/wilson/Main.java
+++ src/wilson/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Aaron.");
-        System.out.println("Hello, Arron my name is Mandi.");
+        System.out.println("Hello, Aaron my name is Mandi.");
+        System.out.println("Hello Aaron, my name is Abram.");
     }
 }
